7722a98b66864a88e7d99c88b2e865bf601958c5
mosab2020 <mabumuammar@students.iugaza.edu.ps>
2023-03-28 14:08:58 +0300

    First Commit

---
+++ app/src/main/java/com/example/assignment03firebaseanalytics/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.assignment03firebaseanalytics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        analytics = Firebase.analytics
+
+        findViewById<Button>(R.id.btn_se).setOnClickListener {
+            selectContentEvent("image1","ImageView","Image")
+        }
+
+        findViewById<Button>(R.id.btn_pe).setOnClickListener {
+            trackScreenEvent()
+        }
+
+    }
+
+    fun selectContentEvent(id: String, name: String, contentType: String) {
+        analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT) {
+            param(FirebaseAnalytics.Param.ITEM_ID, id)
+            param(FirebaseAnalytics.Param.ITEM_NAME, name)
+            param(FirebaseAnalytics.Param.CONTENT_TYPE, contentType)
+        }
+    }
+
+    fun trackScreenEvent() {
+        analytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, "Main")
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, "MainActivity")
+
+        }
+    }
+
+}
